d042ad713663e07f2d2a7464166ba9fff9c6ba9f
dhx <a3237779503@gmail.com>
2026-03-17 21:08:02 +0800

    init project

---
+++ src/test/java/com/haisen/qaknow/QaKnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haisen.qaknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaKnowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
